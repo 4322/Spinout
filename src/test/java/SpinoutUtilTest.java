487bc286a1a2a3ab656ea86b5270bf0e94cf1ae1
zaydkhan4322 <zaydkhan@frc4322.com>
2024-05-03 13:31:37 -0700

    finish writing unit tests for calcWheelToLock

--- src/test/java/SpinoutUtilTest.java
+++ src/test/java/SpinoutUtilTest.java
@@ -63,4 +63,36 @@ public class SpinoutUtilTest {
     assertEquals(Wheel.FL, SpinoutUtil.calcWheelToLock(new Translation2d(0, -1), Rotation2d.fromDegrees(180), SpinoutDirection.CCW, 30));
   }
 
+  @Test
+  public void testCalcWheelToLock_CornerSpans() {
+    assertEquals(Wheel.FL, SpinoutUtil.calcWheelToLock(new Translation2d(1, 0), Rotation2d.fromDegrees(29), SpinoutDirection.CW, 30));
+    assertEquals(Wheel.FR, SpinoutUtil.calcWheelToLock(new Translation2d(1, 0), Rotation2d.fromDegrees(30), SpinoutDirection.CW, 30));
+    assertEquals(Wheel.FR, SpinoutUtil.calcWheelToLock(new Translation2d(1, 0), Rotation2d.fromDegrees(29), SpinoutDirection.CW, 45));
+  }
+
+  @Test
+  public void testCalcWheelToLock_BadCornerSpans() {
+    assertEquals(Wheel.FR, SpinoutUtil.calcWheelToLock(new Translation2d(1, 0), Rotation2d.fromDegrees(45), SpinoutDirection.CW, 0));
+    assertEquals(Wheel.BR, SpinoutUtil.calcWheelToLock(new Translation2d(1, 0), Rotation2d.fromDegrees(45), SpinoutDirection.CCW, 0));
+
+    assertEquals(Wheel.FR, SpinoutUtil.calcWheelToLock(new Translation2d(1, 0), Rotation2d.fromDegrees(0), SpinoutDirection.CW, 90));
+    assertEquals(Wheel.FR, SpinoutUtil.calcWheelToLock(new Translation2d(1, 0), Rotation2d.fromDegrees(0), SpinoutDirection.CCW, 90));
+
+    assertEquals(Wheel.FR, SpinoutUtil.calcWheelToLock(new Translation2d(1, 0), Rotation2d.fromDegrees(0), SpinoutDirection.CW, 91));
+    assertEquals(Wheel.FR, SpinoutUtil.calcWheelToLock(new Translation2d(1, 0), Rotation2d.fromDegrees(0), SpinoutDirection.CCW, 91));
+  }
+
+  @Test
+  public void testCalcWheelToLock_NoInput() {
+    assertEquals(Wheel.FL, SpinoutUtil.calcWheelToLock(new Translation2d(0, 0), Rotation2d.fromDegrees(0), SpinoutDirection.CW, 30));
+    assertEquals(Wheel.FR, SpinoutUtil.calcWheelToLock(new Translation2d(0, 0), Rotation2d.fromDegrees(0), SpinoutDirection.CCW, 30));
+  }
+
+  @Test
+  public void testCalcWheelToLock_RegularInputs() {
+    assertEquals(Wheel.BL, SpinoutUtil.calcWheelToLock(new Translation2d(-0.357, -0.61), Rotation2d.fromDegrees(156), SpinoutDirection.CW, 30));
+    assertEquals(Wheel.FR, SpinoutUtil.calcWheelToLock(new Translation2d(0.19, 0.687), Rotation2d.fromDegrees(-240), SpinoutDirection.CCW, 30));
+    assertEquals(Wheel.FL, SpinoutUtil.calcWheelToLock(new Translation2d(0.157, -0.69), Rotation2d.fromDegrees(296), SpinoutDirection.CW, 30));
+    assertEquals(Wheel.FR, SpinoutUtil.calcWheelToLock(new Translation2d(0.705, -0.055), Rotation2d.fromDegrees(3), SpinoutDirection.CCW, 30));
+  }
 }
